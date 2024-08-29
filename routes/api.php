a44553f15b62ee36f2c7e79cef0da239f724be0d
Bernardo Dartora <bernardintd@gmail.com>
2024-08-29 13:27:02 -0300

    Chamada de rotas da api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MentorController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('register', UserController::class);
+
+Route::apiResource('login', AuthController::class);
+
+Route::apiResource('mentors', MentorController::class)->middleware(['auth:sanctum', 'admin']);
+
+
+
